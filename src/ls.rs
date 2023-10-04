da10d2bfec5c1d04a86e19f4b5f4bd5d5e4b71d2
interpure <anton.stecyura.pr1@gmail.com>
2023-10-04 23:40:52 +0300

    Colorized Ls command output, refactor Ls module

--- src/ls.rs
+++ src/ls.rs
@@ -1,17 +1,23 @@
-use std::fs;
+use std::{fs, io};
 
-pub fn run() {
-    let entries = fs::read_dir("./").unwrap();
+use colored::Colorize;
+
+pub fn run() -> io::Result<()> {
+    let entries = fs::read_dir("./")?;
 
     for entry in entries {
-        let dir_entry = entry.unwrap();
+        let dir_entry = entry?;
 
-        let is_dir = dir_entry.file_type().unwrap().is_dir();
+        let file_type = dir_entry.file_type()?;
+        let file_name = dir_entry.file_name();
 
-        if is_dir { 
-            if let Ok(file_name) = dir_entry.file_name().into_string() {
-                print!("./{}   ", file_name);   
-            }
-        }
-    };    
+        let colored_name = match file_type.is_dir() {
+            true => file_name.to_string_lossy().blue(),
+            false => file_name.to_string_lossy().white(),
+        };
+
+        println!("{}", colored_name);
+    }
+
+    Ok(())
 }
--- src/main.rs
+++ src/main.rs
@@ -1,32 +1,28 @@
 use args::{Cli, Commands};
+
 use clap::Parser;
+use colored::Colorize;
+extern crate colored;
 
 mod args;
 mod cat;
 mod echo;
-mod ls;
 mod grep;
+mod ls;
 
 fn main() {
     let args = Cli::parse();
-
     let command = args.command.expect("Command not valid");
 
     match command {
-        Commands::Cat { path } => {
-            cat::run(cat::Config { file_path: path })
-        },
+        Commands::Cat { path } => cat::run(cat::Config { file_path: path }),
 
-        Commands::Echo { text } => { 
-            echo::run(echo::Config { input: text })
-        },
+        Commands::Echo { text } => echo::run(echo::Config { input: text }),
 
-        Commands::Ls => { 
+        Commands::Ls => {
             ls::run();
-        },
+        }
 
-        Commands::Grep { file_path,query } => { 
-            grep::run(grep::Config::new(file_path, query))
-        },
+        Commands::Grep { file_path, query } => grep::run(grep::Config::new(file_path, query)),
     }
 }
